ad0196489fe0259e220c6788cb0e81ac863da69a
hoekthemad <s.swift.12.35@gmail.com>
2023-09-04 23:04:10 +0100

    turn off line comments. i dont need them

--- style/config.rb
+++ style/config.rb
@@ -9,6 +9,7 @@ images_dir = "images"
 javascripts_dir = "javascripts"
 asset_cache_buster = :none
 cache = false
+line_comments = false
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -17,7 +18,7 @@ cache = false
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# 
 
 
 # If you prefer the indented syntax, you might want to regenerate this
