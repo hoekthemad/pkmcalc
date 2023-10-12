799cd7752534035af4f7b08932c701c24cb971a9
hoekthemad <s.swift.12.35@gmail.com>
2023-10-12 22:04:18 +0100

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,45 @@
+const properties = {
+    effect_type: 'happiness',
+    list: {
+        tent: {
+            base_value: 0,
+            effect: 1
+        },
+        wooden_hut: {
+            base_value: 7.5e+01,
+            effect: 2
+        },
+        cottage: {
+            base_value: 4.69e+04,
+            effect: 3
+        },
+        house: {
+            base_value: 2.93e+06,
+            effect: 4
+        },
+        large_house: {
+            base_value: 1.83e+10,
+            effect: 5
+        },
+        small_palace: {
+            base_value: 1.14e+13,
+            effect: 6
+        },
+        grand_palace: {
+            base_value: 7.15e+15,
+            effect: 7
+        },
+        town_ruler: {
+            base_value: 4.47e+18,
+            effect: 8
+        },
+        city_ruler: {
+            base_value: 2.79e+21,
+            effect: 9
+        },
+        kingdom_minister: {
+            base_value: 6.99e+22,
+            effect: 10
+        }
+    }
+}
