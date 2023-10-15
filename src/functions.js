579be276d8d199ea6b7363a994946f1c8977c21b
hoekthemad <s.swift.12.35@gmail.com>
2023-10-15 21:44:06 +0100

    update skillcalc

--- src/functions.js
+++ src/functions.js
@@ -37,7 +37,7 @@ let calc = {
 
         if (calc.isBoostEnabled) calc.happiness = calc.happiness / 2;
 
-        jQuery("#calculated_skill_price").html(getSkillPrice(
+        let skillPrice = Math.floor(getSkillPrice(
             calc.targetSkillLevel-1,
             isTheOrder,
             calc.allPriceModifier,
@@ -47,5 +47,79 @@ let calc = {
             calc.fana,
             calc.isBoostEnabled
         ));
+
+        let splitPrice = skillPrice.toString().split("");
+
+        let count = 0;
+        let theprice = "";
+        for (let i = splitPrice.length; i > 0; i--) {
+            if (0 == count) {
+                theprice = theprice+"c";
+            }
+            else if (2 == count) {
+                theprice = "s "+theprice;
+            }
+            else if (4 == count) {
+                theprice = "g "+theprice;
+            }
+            else if (6 == count) {
+                theprice = "p "+theprice;
+            }
+            else if (8 == count) {
+                theprice = "e "+theprice;
+            }
+            else if (10 == count) {
+                theprice = "a "+theprice;
+            }
+            else if (12 == count) {
+                theprice = "r "+theprice;
+            }
+            else if (14 == count) {
+                theprice = "t "+theprice;
+            }
+            else if (16 == count) {
+                theprice = "d "+theprice;
+            }
+            theprice = splitPrice[i-1]+theprice;
+            count++;
+        }
+
+
+        jQuery("#calculated_skill_price").html(theprice);
+    }
+}
+
+let dataImporter = {
+    decode: (base64) => {
+        const binString = atob(base64);
+        let ret = JSON.parse(binString);
+        return ret;
+    },
+
+    forSkills: () => {
+        let b64string = $("#base64stats").val();
+        if (b64string.length == 0) {
+            alert("Please enter your stats");
+            return;
+        }
+        let stats = dataImporter.decode(b64string);
+        console.log(stats);
+
+        let dateNow = Date.now();
+        let happinessBoostEnd = Date.parse(stats['boosts']['happiness']);
+
+        let happyBoostActive = false;
+        if (happinessBoostEnd > dateNow)
+            happyBoostActive = true;
+
+        let happiness = 1*stats['stats']['happiness'];
+        if (true == happyBoostActive) {
+            happiness = happiness*2;
+            $("#is_boost_active").prop("checked", "checked");
+        }
+
+        $("#price_modifier").val(happiness);
+        $("#happiness_modifier").val(stats['stats']['allPrices']);
+        $("#fanaticism").val(stats['fanaticism']);
     }
 }
