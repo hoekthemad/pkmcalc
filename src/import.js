297dd35d2dfc1dea5aeef3e047f44dd34bf98287
hoekthemad <s.swift.12.35@gmail.com>
2023-11-16 23:48:24 +0000

    Update import.js

--- src/import.js
+++ src/import.js
@@ -1,5 +1,15 @@
 let _import = {
     allstats: {
+        balance: 0,
+        fanaticism: 0,
+        brands: 0,
+        boosts: {
+            income: {
+                ad: Date.now(),
+                tavern: Date.now()
+            },
+            happiness: Date.now()
+        },
         skills: {
             theorder: {
                 faith:      {   level: 1, reliclevel: 1, effect: 0.205    },
@@ -79,69 +89,66 @@ let _import = {
         },
 
     },
-    stats: {
-        income: 1,
-        happiness: 1,
-        allprices: 1,
-        shopprices: 1,
-        skilleffects: 1,
-        bonusrelic: 1,
-        flatincome: 1,
-        atkduration: 1,
-        enatcduration: 1,
-        atkpower: 1,
-        atkresistance: 1,
-        insanitythreshold: 1,
-        
-        misc: {
-            biggestfishever: 0,
-            smallestfishever: 0,
-            fish: 0,
-            smallfish: 0,
-            weeklyfishcaught: 0,
-            weeklyfishattempt: 0,
-            totalfishcaught: 0,
-            shifumiwon: 0,
-            shifumilost: 0,
-            shifumiplayed: 0,
-            totalspent: 0,
-            totalstolen: 0,
-            weeklyspent: 0,
-            weeklystolen: 0,
-            weeklygotstolen: 0,
-            totalattacks: 0,
-            weeklyattacks: 0,
-            weeklygotattacked: 0,
-            weeklysacrifice: 0,
-            lastsacrifice: Date.now(),
-            lastsurrender: Date.now(),
-            lasttouchrelic: Date.now(),
-            maxfanaticism: 0,
-            totalfanaticism: 0,
-            totalsacrifice: 0,
-            totalsurrender: 0,
-            lastweeklyspent: 0,
-            lastweeklysacrifice: 0,
-            accountcreatedat: Date.now()
-        }
-    },
-
+    
     go: (data) => {
+        var stats;
         if (10 > data.toString().length) {
             jQuery.ajax({
                 url: "http://api.ezagdev.net/player-data/"+data,
                 type: "GET",
                 async: false,
                 success: function (res) {
-                    _import.stats = res;
-                    console.log(_import.stats);
+                    stats = res;
                 }
             });
         }
         else {
             const binString = atob(data.replace(/[^\x00-\x7F]/g, ""));
-            _import.stats = JSON.parse(binString);
-            console.log(_import.stats);
+            stats = JSON.parse(binString);
+        }
+
+        let shop = stats.playerData.shop;
+        {
+            _import.allstats.shop.properties.tent = shop['tent'] ?? false;
+            _import.allstats.shop.properties.woodenhut = shop['wooden hut'] ?? false;
+            _import.allstats.shop.properties.cottage = shop['cottage'] ?? false;
+            _import.allstats.shop.properties.house = shop['house'] ?? false;
+            _import.allstats.shop.properties.largehouse = shop['large house'] ?? false;
+            _import.allstats.shop.properties.smallpalace = shop['small palace'] ?? false;
+            _import.allstats.shop.properties.grandpalace = shop['grand palace'] ?? false;
+            _import.allstats.shop.properties.townruler = shop['town ruler'] ?? false;
+            _import.allstats.shop.properties.cityruler = shop['city ruler'] ?? false;
+            _import.allstats.shop.properties.kingdomminister = shop['kindom minister'] ?? false;
+        }
+        {
+            _import.allstats.shop.trinkets.coinpouch = shop['coin pouch'] ?? false;
+            _import.allstats.shop.trinkets.woodencrown = shop['wooden crown'] ?? false;
+            _import.allstats.shop.trinkets.dumbbells = shop['dumbbells'] ?? false;
+            _import.allstats.shop.trinkets.luckycharm = shop['lucky charm'] ?? false;
+            _import.allstats.shop.trinkets.pendulum = shop['pendulum'] ?? false;
+            _import.allstats.shop.trinkets.silverring = shop['silver ring'] ?? false;
+            _import.allstats.shop.trinkets.ceremonyknife = shop['ceremony knife'] ?? false;
+            _import.allstats.shop.trinkets.magicpebbles = shop['magic pebbles'] ?? false;
+            _import.allstats.shop.trinkets.shinylamp = shop['shiny lamp'] ?? false;
+            _import.allstats.shop.trinkets.goldenhourglass = shop['golden hourglass'] ?? false;
+        }
+        {
+            _import.allstats.shop.weapons.smallshield = shop['small shield'] ?? false;
+            _import.allstats.shop.weapons.warpaint = shop['war paint'] ?? false;
+            _import.allstats.shop.weapons.shortbow = shop['short bow'] ?? false;
+            _import.allstats.shop.weapons.steellongsword = shop['steel longsword'] ?? false;
+            _import.allstats.shop.weapons.knightarmor = shop['knight armor'] ?? false;
+            _import.allstats.shop.weapons.warhorse = shop['war horse'] ?? false;
+            _import.allstats.shop.weapons.magicsword = shop['magic sword'] ?? false;
+        }
+        {
+            _import.allstats.shop.servants.squire = shop['squire'] ?? false;
+            _import.allstats.shop.servants.bookkeeper = shop['bookkeeper'] ?? false;
+            _import.allstats.shop.servants.butler = shop['butler'] ?? false;
+            _import.allstats.shop.servants.banker = shop['banker'] ?? false;
+            _import.allstats.shop.servants.seer = shop['seer'] ?? false;
+            _import.allstats.shop.servants.bodyguard = shop['bodyguard'] ?? false;
+            _import.allstats.shop.servants.holyman = shop['holyman'] ?? false;
         }
     }
 }
