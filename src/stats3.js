60230c96723c93e80dc3eccc307399a519a844e2
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-02-19 12:14:52 +0000

    Update stats3.js

--- src/stats3.js
+++ src/stats3.js
@@ -128,10 +128,13 @@ let StatPage = {
 
         for (group = 0; group < StatPage.groupHTMLNames.length; group++) {
             for (skill = 0; skill < StatPage.skills[StatPage.groupHTMLNames[group]].length; skill++) {
-                jQuery(`#${StatPage.skills[skill]}_skill_level`).val(stats.skills[StatPage.groupHTMLNames[group]][skill].level);
-                jQuery(`#${StatPage.skills[skill]}_relic_level`).val(stats.skills[StatPage.groupHTMLNames[group]][skill].reliclevel);
-                jQuery(`#${StatPage.skills[skill]}_skill_effect`).text(getEffect(1*skillEffects[group][skill], 1*stats[group][skill].level, 1*stats.playerStats.skilleffects, false));
-                updateSkill(skill);
+                let groupName = StatPage.groupHTMLNames[group];
+                let skillName = StatPage.skills[groupname][skill];
+                
+                jQuery(`#${StatPage.skills[skill]}_skill_level`).val(stats.skills[groupName][skillName].level);
+                //jQuery(`#${StatPage.skills[skill]}_relic_level`).val(stats.skills[groupName][skillName].reliclevel);
+                //jQuery(`#${StatPage.skills[skill]}_skill_effect`).text(getEffect(1*skillEffects[group][skill], 1*stats[group][skill].level, 1*stats.playerStats.skilleffects, false));
+                //updateSkill(skill);
             }
         }
     }
