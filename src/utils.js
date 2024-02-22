bd45ebffff0e3f6727577adfc1fbe19c958410f6
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-02-22 13:43:17 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -43,15 +43,16 @@ let convertIntToCurrency = (number) => {
     if (fullSplit.length >= 3) thereturn = `${fullSplit[0]} ${fullSplit[1]} ${fullSplit[2]}`;
     if (fullSplit.length >= 2) thereturn = `${fullSplit[0]} ${fullSplit[1]}`;
     if (fullSplit.length >= 1) thereturn = `${fullSplit[0]}`;
-    thereturn = thereturn.replace(/([0-9]{1,2}c)/g, "<span class='copper'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}s)/g, "<span class='silver'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}g)/g, "<span class='gold'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}p)/g, "<span class='platinum'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}e)/g, "<span class='emerald'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}a)/g, "<span class='amethyst'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}r)/g, "<span class='ruby'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}t)/g, "<span class='topaz'>$1</span>");
-    thereturn = thereturn.replace(/([0-9]{1,2}d)/g, "<span class='diamond'>$1</span>");
+    let retval = "";
+    retval = retval + thereturn.replace(/([0-9]{1,2}c)/g, "<span class='copper'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}s)/g, "<span class='silver'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}g)/g, "<span class='gold'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}p)/g, "<span class='platinum'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}e)/g, "<span class='emerald'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}a)/g, "<span class='amethyst'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}r)/g, "<span class='ruby'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}t)/g, "<span class='topaz'>$1</span>");
+    retval = retval + thereturn.replace(/([0-9]{1,2}d)/g, "<span class='diamond'>$1</span>");
 
     return thereturn;
 }
