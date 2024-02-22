f553a4c7f2e02efebe6a94b82926d60df0a78030
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-02-22 13:29:51 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -2,47 +2,58 @@ let convertIntToCurrency = (number) => {
     if (Infinity == number) return "infinity";
     let splitPrice = number.toString().split("");
 
-        let count = 0;
-        let theprice = "";
-        for (let i = splitPrice.length; i > 0; i--) {
-            if (0 == count) {
-                theprice = theprice+"c";
-            }
-            else if (2 == count) {
-                theprice = "s "+theprice;
-            }
-            else if (4 == count) {
-                theprice = "g "+theprice;
-            }
-            else if (6 == count) {
-                theprice = "p "+theprice;
-            }
-            else if (8 == count) {
-                theprice = "e "+theprice;
-            }
-            else if (10 == count) {
-                theprice = "a "+theprice;
-            }
-            else if (12 == count) {
-                theprice = "r "+theprice;
-            }
-            else if (14 == count) {
-                theprice = "t "+theprice;
-            }
-            else if (16 == count) {
-                theprice = "d "+theprice;
-            }
-            theprice = splitPrice[i-1]+theprice;
-            count++;
+    let count = 0;
+    let theprice = "";
+    for (let i = splitPrice.length; i > 0; i--) {
+        if (0 == count) {
+            theprice = theprice+"c";
         }
+        else if (2 == count) {
+            theprice = "s "+theprice;
+        }
+        else if (4 == count) {
+            theprice = "g "+theprice;
+        }
+        else if (6 == count) {
+            theprice = "p "+theprice;
+        }
+        else if (8 == count) {
+            theprice = "e "+theprice;
+        }
+        else if (10 == count) {
+            theprice = "a "+theprice;
+        }
+        else if (12 == count) {
+            theprice = "r "+theprice;
+        }
+        else if (14 == count) {
+            theprice = "t "+theprice;
+        }
+        else if (16 == count) {
+            theprice = "d "+theprice;
+        }
+        theprice = splitPrice[i-1]+theprice;
+        count++;
+    }
 
-        //return theprice;
+    //return theprice;
 
-        let fullSplit = theprice.split(" ");
+    let fullSplit = theprice.split("</span>");
 
-        if (fullSplit.length >= 3) return `${fullSplit[0]} ${fullSplit[1]} ${fullSplit[2]}`;
-        if (fullSplit.length >= 2) return `${fullSplit[0]} ${fullSplit[1]}`;
-        if (fullSplit.length >= 1) return `${fullSplit[0]}`;
+    if (fullSplit.length >= 3) thereturn = `${fullSplit[0]}</span> ${fullSplit[1]}</span> ${fullSplit[2]}</span>`;
+    if (fullSplit.length >= 2) thereturn = `${fullSplit[0]}</span> ${fullSplit[1]}`;
+    if (fullSplit.length >= 1) thereturn = `${fullSplit[0]}`;
+    thereturn = thereturn.replace(/([0-9]{1,2}c)/, "<span class='copper'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}s)/, "<span class='silver'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}g)/, "<span class='gold'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}p)/, "<span class='platinum'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}e)/, "<span class='emerald'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}a)/, "<span class='amethyst'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}r)/, "<span class='ruby'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}t)/, "<span class='topaz'>$1</span>");
+    thereturn = thereturn.replace(/([0-9]{1,2}d)/, "<span class='diamond'>$1</span>");
+
+    return thereturn;
 }
 
 let checkIsOrder = (skillname) => {
