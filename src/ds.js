fed53c521bf99d3da53af94f971b84b8bfa3d0a2
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-02-23 08:55:13 +0000

    Update ds.js

--- src/ds.js
+++ src/ds.js
@@ -2,47 +2,58 @@ data = {
     properties: {
         tent: {
             base_price: 0,
-            effect: 1
+            effect: 1,
+            display: "Tent"
         },
         wooden_hut: {
             base_price: 75,
-            effect: 2
+            effect: 2,
+            display: "Wooden Hut"
         },
         cottage: {
             base_price: 46875,
-            effect: 3
+            effect: 3,
+            display: "Cottage"
         },
         house: {
             base_price: 29296875,
-            effect: 4
+            effect: 4,
+            display: "House"
         },
         large_house: {
             base_price: 18310546875,
-            effect: 5
+            effect: 5,
+            display: "Large House"
         },
         small_palace: {
             base_price: 11444091796875,
-            effect: 6
+            effect: 6,
+            display: "Small Palace"
         },
         grand_palace: {
             base_price: 7152557373046875,
-            effect: 7
+            effect: 7,
+            display: "Grand Palace"
         },
         town_ruler: {
             base_price: 4470348358154296300n,
-            effect: 8
+            effect: 8,
+            display: "Town Ruler"
         },
         city_ruler: {
             base_price: 2793967723846436000000n,
-            effect: 9
+            effect: 9,
+            display: "City Ruler"
         },
         kingdom_minister: {
             base_price: 1746229827404022300000000n,
-            effect: 10
+            effect: 10,
+            display: "Kingdom Minister"
         },
         heaven: {
             base_price: 218278728425502760000000000n,
-            effect: 10
+            effect: 10,
+            display: "Heaven"
         }
     },
     trinkets: {
@@ -50,140 +61,167 @@ data = {
             effect_type: "income",
             effect_value: 2.5,
             base_price: 59,
+            display: "Coin Pouch"
         },
         wooden_crown: {
             effect_type: "shopprices",
             effect_value: 2,
             base_price: 166772,
+            display: "Wooden Hut"
         },
         dumbells: {
             effect_type: "atkduration",
             effect_value: 1.5,
             base_price: 8862938,
+            display: "Dumbells"
         },
         lucky_charm: {
             effect_type: "income",
             effect_value: 3,
             base_price: 471012870,
+            display: "Lucky Charm"
         },
         soulbound_locket: {
             effect_type: "greed",
             effect_value: 2,
-            base_price: 1507241183
+            base_price: 1507241183,
+            display: "Soulbound Locket"
         },
         pendulum: {
             effect_type: "bonusrelic",
             effect_value: 0.2,
             base_price: 25031555050,
+            display: "Pendulum"
         },
         silver_ring: {
             effect_type: "allprices",
             effect_value: 2,
             base_price: 1330279464729,
+            display: "Silver Ring"
         },
         ceremony_knife: {
             effect_type: "happiness",
             effect_value: 1.5,
             base_price: 70696504901511,
+            display: "Ceremony Knife"
         },
         magic_pebbles: {
             effect_type: "shopprices",
             effect_value: 1.5,
             base_price: 3757102126136372,
+            display: "Magic Pebbles"
         },
         shiny_lamp: {
             effect_type: "allprices",
             effect_value: 2,
             base_price: 199667811101603970n,
+            display: "Shiny Lamp"
         },
         golden_hourglass: {
             effect_type: "bonusrelic",
             effect_value: 1.5,
             base_price: 10611166119964754000n,
+            display: "Golden Hourglass"
         },
         mystic_satchel: {
             effect_type: "greed",
             effect_value: 1.5,
-            base_price: 563920873396019130000n
+            base_price: 563920873396019130000n,
+            display: "Mystic Satchel"
         }
     },
     weapons: {
         small_shield: {
             effect_type: "enatkduration",
             effect_value: 2,
-            base_price: 626
+            base_price: 626,
+            display: "Small Shield"
         },
         war_paint: {
             effect_type: "atkresistance",
             effect_value: 1.5,
-            base_price: 130434
+            base_price: 130434,
+            display: "War Paint"
         },
         short_bow: {
             effect_type: "enatkduration",
             effect_value: 2,
-            base_price: 27197214
+            base_price: 27197214,
+            display: "Short Bow"
         },
         steel_longsword: {
             effect_type: "atkduration",
             effect_value: 2,
-            base_price: 5670988944
+            base_price: 5670988944,
+            display: "Steel Longsowrd"
         },
         knight_armor: {
             effect_type: "atkresistance",
             effect_value: 1.5,
-            base_price: 1182478320247
+            base_price: 1182478320247,
+            display: "Knight Armor"
         },
         war_horse: {
             effect_type: "atkpower",
             effect_value: 2,
-            base_price: 246562811476569
+            base_price: 246562811476569,
+            display: "War Horse"
         },
         magic_sword: {
             effect_type: "shopprices",
             effect_value: 1.5,
-            base_price: 51411699447100780n
+            base_price: 51411699447100780n,
+            display: "Magic Sword"
         }
     },
     servants: {
         squire: {
             effect_type: "shopprices",
             effect_value: 1.5,
-            base_price: 3476
+            base_price: 3476,
+            display: "Squire"
         },
         bookeeper: {
             effect_type: "income",
             effect_value: 2,
-            base_price: 982146
+            base_price: 982146,
+            display: "Bookkeeper"
         },
         butler: {
             effect_type: "happiness",
             effect_value: 1.5,
-            base_price: 277530931
+            base_price: 277530931,
+            display: "Butler"
         },
         banker: {
             effect_type: "allprices",
             effect_value: 2,
-            base_price: 22160637303640
+            base_price: 22160637303640,
+            display: "Banker"
         },
         seer: {
             effect_type: "bonusrelic",
             effect_value: 1,
-            base_price: 6262066462777030
+            base_price: 6262066462777030,
+            display: "Seer"
         },
         bodyguard: {
             effect_type: "atkresistance",
             effect_value: 2,
-            base_price: 1769510318992327700n
+            base_price: 1769510318992327700n,
+            display: "Bodyguard"
         },
         holyman: {
             effect_type: "shopprices",
             effect_value: 1.5,
-            base_price: 500021324850607700000n
+            base_price: 500021324850607700000n,
+            display: "Holy Man"
         },
         smuggler: {
             effect_type: "greed",
             effect_value: 1.5,
-            base_price: 141294075893117760000000n
+            base_price: 141294075893117760000000n,
+            display: "Smuggler"
         }
     },
 
