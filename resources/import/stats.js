e16b74763c75a1ba5e60015d411b30555e711c77
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-02-26 14:47:42 +0000

    update

---
+++ resources/import/stats.js
@@ -0,0 +1,269 @@
+let player = {
+    "playerData": {
+        "balance": 1,
+        "fanaticism": 0,
+        "relicTouches": 1,
+        "brands": 0,
+        "boosts": {
+            "income": [
+                Date.now(),
+                Date.now()
+            ],
+            "happiness": [
+                Date.now()
+            ]
+        },
+        "skills": {
+            "productivity": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "bargaining": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "concentration": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "meditation": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "strength": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "battle tactics": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "muscle memory": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "mana control": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "life essence": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "fanatical devotion": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "zealous conviction": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "devout mastery": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "ardent belief": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "extreme piety": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "absolute faith": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "dogged perseverance": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "blazing fervor": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "faith": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "devotion": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "fervour": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "zeal": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "resilience": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            },
+            "materialism": {
+                "level": 1,
+                "relicLevel": 1,
+                "value": 1
+            }
+        },
+        "insanity": 0,
+        "shop": {
+            "wooden crown": false,
+            "dumbbells": false,
+            "small shield": false,
+            "short bow": false,
+            "butler": false,
+            "wooden hut": false,
+            "cottage": false,
+            "tent": false,
+            "house": false,
+            "large house": false,
+            "small palace": false,
+            "grand palace": false,
+            "town ruler": false,
+            "city ruler": false,
+            "lucky charm": false,
+            "bookkeeper": false,
+            "banker": false,
+            "war paint": false,
+            "silver ring": false,
+            "ceremony knife": false,
+            "magic pebbles": false,
+            "shiny lamp": false,
+            "steel longsword": false,
+            "knight armor": false,
+            "war horse": false,
+            "magic sword": false,
+            "bodyguard": false,
+            "squire": false,
+            "holy man": false,
+            "coin pouch": false,
+            "kingdom minister": false,
+            "golden hourglass": false,
+            "pendulum": false,
+            "seer": false,
+            "smuggler": false,
+            "mystic satchel": false,
+            "heaven": false,
+            "soulbound locket": false
+        },
+        "stats": {
+            "income": 1,
+            "happiness": 1,
+            "allPrices": 1,
+            "shopPrices": 1,
+            "skillEffects": 1,
+            "bonusRelic": 0,
+            "flatIncome": 0,
+            "atkDuration": 1,
+            "enAtkDuration": 1,
+            "atkPower": 1,
+            "atkResistance": 1,
+            "insanityThreshold": 1,
+            "fosBonus": 0,
+            "brandsBonus": 0,
+            "tableBonus": 0
+        },
+        "misc": {
+            "biggestFishEver": 0,
+            "smallestFishEver": 0,
+            "fish": 0,
+            "smallFish": 0,
+            "weeklyFishCaught": 0,
+            "weeklyFishAttempt": 0,
+            "totalFishCaught": 0,
+            "totalFishAttempt": 0,
+            "shifumiWon": 0,
+            "shifumiLost": 0,
+            "shifumiPlayed": 0,
+            "totalSpent": 0,
+            "totalStolen": 0,
+            "weeklySpent": 0,
+            "lastWeeklySpent": "0",
+            "weeklyStolen": 0,
+            "weeklyGotStolen": 0,
+            "totalAttacks": 0,
+            "weeklyAttacks": 0,
+            "weeklyGotAttacked": 0,
+            "weeklySacrifice": 0,
+            "weeklyFanaticism": 0,
+            "lastSacrifice": false,
+            "lastSurrender": false,
+            "lastTouchRelic": false,
+            "maxFanaticism": 0,
+            "totalFanaticism": 0,
+            "totalSacrifice": 0,
+            "totalSurrender": 0,
+            "totalTouchRelic": 0,
+            "lastWeeklySacrifice": "0",
+            "createdAt": Date.now()
+        },
+        "fos": 0,
+        "talents": [
+            [
+                1
+            ],
+            [
+                0,
+                1
+            ],
+            [
+                0,
+                1,
+                1
+            ],
+            [
+                0,
+                1,
+                1,
+                0,
+                1
+            ],
+            [
+                1,
+                1
+            ],
+            [
+                0,
+                1,
+                1
+            ],
+            [
+                0,
+                0,
+                1,
+                0,
+                1
+            ],
+            [
+                12,
+                0,
+                12
+            ]
+        ]
+    }
+}
--- resources/statcalculations.js
+++ resources/statcalculations.js
@@ -13,3 +13,63 @@
  * - insanity threshold
  * - greed (halp)
  */
+
+const getHappiness = () => {
+    let happiness = 1;
+    return happiness;
+}
+
+const getAllPrice = () => {
+    let allPrice = 1;
+    return allPrice;
+}
+
+const getShopPrice = () => {
+    let shopPrice = 1;
+    return shopPrice;
+}
+
+const getIncome = () => {
+    let income = 1;
+    return income;
+}
+
+const getSkillEffect = () => {
+    let skillEffect = 1;
+    return skillEffect;
+}
+
+const getBonusRelicLevels = () => {
+    let bonusRelicLevel = 1;
+    return bonusRelicLevel > 1 ? bonusRelicLevel : 0;
+}
+
+const getAttackDuration = () => {
+    let attackDuration = 1;
+    return attackDuration;
+}
+
+const getEnemtyAttackDuration = () => {
+    let enemyAttackDuration = 1;
+    return enemyAttackDuration;
+}
+
+const getAttackPower = () => {
+    let attackPower = 1;
+    return attackPower;
+}
+
+const getAttackResistance = () => {
+    let attackResistance = 1;
+    return attackResistance;
+}
+
+const getInsanityThreshold = () => {
+    let insanityThreshold = 1;
+    return insanityThreshold;
+}
+
+const getInsanityThresholdReduction = () => {
+    let insanityThresholdReduction = 1;
+    return insanityThresholdReduction;
+}
