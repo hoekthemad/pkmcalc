4b07b6db7a8058faadffce097329ded86571dadd
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-02-26 09:19:48 +0000

    Update skills.js

---
+++ resources/data/skills.js
@@ -0,0 +1,49 @@
+let skills= {
+    theorder: {
+        faith: {
+            effects: effectTypes.income,
+            effectvalue: 0.205,
+            baseprice: 1,
+            io: {
+                html: "faith", display: "Faith", import: "faith"
+            }
+        },
+        zeal: {
+            effects: effectTypes.allprice,
+            effectvalue: 0.12,
+            baseprice: 1,
+            io: {
+                html: "zeal", "display": "Zeal", import: "zeal"
+            }
+        },
+        devotion: 0.135,
+        fervour: 0.0135
+    },
+    fundamentals: {
+        productivity: 0.03,
+        concentration: 0.01,
+        bargaining: 0.01,
+        meditation: 0.01
+    },
+    combat: {
+        strength: 0.015,
+        battletactics: 0.01,
+        musclememory: 0.01
+    },
+    magic: {
+        manacontrol: 0.01,
+        lifeessence: 0.02,
+        resilience: 0.005,
+        materialism: 0.04
+    },
+    darkmagic: {
+        fanaticaldevotion: 0.01,
+        ardentbelief: 0.03,
+        zealousconviction: 0.01,
+        extremepiety: 0.01,
+        absolutefaith: 0.01,
+        devoutmastery: 0.002,
+        doggedperseverance: 0.003,
+        blazingfervour: 0.0027
+    }
+};
