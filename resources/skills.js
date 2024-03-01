6ce4328ebf32ce20a0411ed99b480dca0cf4eb12
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-03-01 11:32:01 +0000

    Test

--- resources/skills.js
+++ resources/skills.js
@@ -1,5 +1,71 @@
 let skills = {
     init: () => {
-        
+        let theorder = _skills.theorder;
+        let fundamentals = _skills.fundamentals;
+        let combat = _skills.combat;
+        let magic = _skills.magic;
+        let darkmagic = _skills.darkmagic;
+
+        let orderGroupHTML = ``;
+        for (let [name, values] of Object.entries(theorder)) {
+            let htmlSkillName = values.io.html;
+            let displaySkillName = values.io.display
+            let importSkillName = values.io.import;
+            let baseSkillEffect = values.effectvalue;
+
+            let skillLevel = Player.skills[importSkillName].level;
+            let relicLevel = Player.skills[importSkillName].relicLevel;
+
+            let skillCost = getSkillPrice(
+                skillLevel, 
+                true, 
+                getAllPrice(), 
+                getHappiness(),
+                relicLevel,
+                false,
+                Player.fanatacism,
+                getBoostStatus('happiness')
+            );
+            let skillEffect = getEffect(
+                baseSkillEffect,
+                skillLevel,
+                getSkillEffect(),
+                false
+            );
+
+            let orderGroupHTML = orderGroupHTML + `
+                <div class="col-auto col-xs col-3 col-sm col-md col-lg col-xl col-xxl align-self-center">
+                    ${displaySkillName}:
+                </div>
+                <div class="col-auto col-xs col-3 col-sm col-md col-lg col-xl col-xxl">
+                    <div class="form-floating">
+                        <input type="number" class="form-control" id="${htmlSkillName}-skill-level" placeholder="1" value="${skillLevel}">
+                        <label for="${htmlSkillName}-skill-level">Skill Level</label>
+                    </div>
+                </div>
+                <div class="col-auto col-xs col-3 col-sm col-md col-lg col-xl col-xxl">
+                    <div class="form-floating">
+                        <input type="number" class="form-control" id="${htmlSkillName}-relic-level" placeholder="1" value="${relicLevel}">
+                        <label for="${htmlSkillName}-relic-level">Relic Level</label>
+                    </div>
+                </div>
+                <div class="col-auto col-xs col-3 col-sm col-md col-lg col-xl col-xxl align-self-center">
+                    ×<span id="${htmlSkillName}-skill-effect">${skillEffect}</span>
+                </div>
+                <div class="col-auto col-xs col-3 col-sm col-md col-lg col-xl col-xxl align-self-center">
+                    <span id="${htmlSkillName}-skill-price">${skillCost}</span>
+                </div><br/>
+            `;
+        }
+
+        let theOrderHTML = `
+            <div id="theorder">
+                <hr class="bg-primary   border border-1-light border border-1-top border border-1-primary" />
+                <div class="row border border-1 rounded-2 border border-1-light border border-1-primary">
+                ${orderGroupHTML}
+                </div>
+            </div>
+            <hr class="bg-danger   border border-1-light border border-1-top border border-1-danger" />
+        `;
     }
 }
