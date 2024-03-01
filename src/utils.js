cc25f45fb49b053671d83d10a615f9950cd3adfb
hoekthemad <s.swift.12.35@gmail.com>
2024-03-01 22:09:16 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-let convertIntToCurrency = (number, displayCount) => {
+const convertIntToCurrency = (number, displayCount) => {
     displayCount = displayCount || 3;
     if (Infinity == number) return "infinity";
     let splitPrice = number.toString().split("");
@@ -49,17 +49,16 @@ let convertIntToCurrency = (number, displayCount) => {
 
     let fullSplit = theprice.split("</span>");
 
-    for (let c = 0; c < displayCount; c++) {
-        if (c == fullSplit.length) break;
-        thereturn = `${fullSplit[c]}</span>`;
-    }
+    let thereturn = '';
 
-    // if (fullSplit.length >= 3) thereturn = `${fullSplit[0]}</span>${fullSplit[1]}</span>${fullSplit[2]}</span>`;
-    // else if (fullSplit.length >= 2) thereturn = `${fullSplit[0]}</span>${fullSplit[1]}</span>`;
-    // else if (fullSplit.length >= 1) thereturn = `${fullSplit[0]}</span>`;
+    for (let c = 0; c < displayCount; c++) {
+        if ((c+1) == fullSplit.length) break;
+        thereturn = `${thereturn}${fullSplit[c]}</span>`;
+    }
 
     return thereturn;
 }
+
 
 let checkIsOrder = (skillname) => {
     if (
