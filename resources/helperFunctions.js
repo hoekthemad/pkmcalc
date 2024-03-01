576d901b06bd1cfcdb762df109f6d27477ce8f35
hoekthemad <144025118+hoekthemad@users.noreply.github.com>
2024-03-01 11:59:25 +0000

    Update helperFunctions.js

--- resources/helperFunctions.js
+++ resources/helperFunctions.js
@@ -66,3 +66,66 @@ const getTextValue = (name, numeric) => {
     if (numeric) return 1*text;
     return text;
 }
+
+let convertIntToCurrency = (number, displayCount) => {
+    displayCount = displayCount || 3;
+    if (Infinity == number) return "infinity";
+    let splitPrice = number.toString().split("");
+
+    let count = 0;
+    let theprice = "";
+    for (let i = splitPrice.length; i > 0; i--) {
+        if (0 == count) {
+            theprice = theprice + "c</span>";
+        }
+        else if (2 == count) {
+            theprice = "s</span> <span class='copper'>" + theprice;
+        }
+        else if (4 == count) {
+            theprice = "g</span> <span class='silver'>" + theprice;
+        }
+        else if (6 == count) {
+            theprice = "p</span> <span class='gold'>" + theprice;
+        }
+        else if (8 == count) {
+            theprice = "e</span> <span class='platinum'>" + theprice;
+        }
+        else if (10 == count) {
+            theprice = "a</span> <span class='emerald'>" + theprice;
+        }
+        else if (12 == count) {
+            theprice = "r</span> <span class='amethyst'>" + theprice;
+        }
+        else if (14 == count) {
+            theprice = "t</span> <span class='ruby'>" + theprice;
+        }
+        else if (16 == count) {
+            theprice = "d</span> <span class='topaz'>" + theprice;
+        }
+        theprice = splitPrice[i - 1] + theprice;
+        count++;
+    }
+
+    if (count == number.toString().length && count > 16) theprice = "<span class='diamond'>" + theprice;
+    if (count == number.toString().length && count > 14 && count <= 16) theprice = "<span class='topaz'>" + theprice;
+    if (count == number.toString().length && count > 12 && count <= 14) theprice = "<span class='ruby'>" + theprice;
+    if (count == number.toString().length && count > 10 && count <= 12) theprice = "<span class='amethyst'>" + theprice;
+    if (count == number.toString().length && count > 8 && count <= 10) theprice = "<span class='emerald'>" + theprice;
+    if (count == number.toString().length && count > 6 && count <= 8) theprice = "<span class='platinum'>" + theprice;
+    if (count == number.toString().length && count > 4 && count <= 6) theprice = "<span class='gold'>" + theprice;
+    if (count == number.toString().length && count > 2 && count <= 4) theprice = "<span class='silver'>" + theprice;
+    if (count == number.toString().length && count > 0 && count <= 2) theprice = "<span class='copper'>" + theprice;
+
+    let fullSplit = theprice.split("</span>");
+
+    for (let c = 0; c < displayCount; c++) {
+        if (c == fullSplit.length) break;
+        thereturn = `${fullSplit[c]}</span>`;
+    }
+
+    // if (fullSplit.length >= 3) thereturn = `${fullSplit[0]}</span>${fullSplit[1]}</span>${fullSplit[2]}</span>`;
+    // else if (fullSplit.length >= 2) thereturn = `${fullSplit[0]}</span>${fullSplit[1]}</span>`;
+    // else if (fullSplit.length >= 1) thereturn = `${fullSplit[0]}</span>`;
+
+    return thereturn;
+}
